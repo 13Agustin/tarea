523bf696ac1f5013047b04155f1e0d7e15c46ab9
Agustin Aguilar <agustin.equila@galileo.edu>
2022-02-04 21:59:24 -0600

    estaciones del año

---
+++ estaciones.js
@@ -0,0 +1,20 @@
+var mes= 12;
+
+if(mes == 12 || mes == 1 ||mes ==2 )
+  console.log("invierno");
+  
+else
+
+if(mes == 3 || mes == 4 ||mes == 5)
+  console.log("primavera");
+
+else
+if(mes == 6 || mes == 7 ||mes == 8)
+  console.log("verano");
+
+else
+
+if(mes == 9|| mes == 10 ||mes == 11)
+  console.log("otoño");
+
+console.log("nombre;Agustin aguilar carnet:22002782");
